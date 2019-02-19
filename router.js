f05fd7935273681664a202c8ae7c37e2d58293e5
J A <jarata@gmail.com>
2019-02-19 15:25:29 -0800

    /api/posts properly PUT

--- router.js
+++ router.js
@@ -48,10 +48,23 @@ router.post('/', async (req, res) => {
         })
     }
 });
-// router.put('/:id', async (req, res) => {
-//
-// });
-
+router.put('/:id', async (req, res) => {
+    try {
+        const post = await Posts.update(req.params.id, req.body)
+        if (post) {
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist."
+            })
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            error: "The post information could not be modified."
+        })
+    }
+});
 router.delete('/:id', async (req, res) => {
     try {
         const count = await Posts.remove(req.params.id)
